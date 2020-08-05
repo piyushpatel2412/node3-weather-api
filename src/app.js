ddf07355c2bbb1862ffc2126ee57bd4b18a577d0
Piyush <200piyush@gmail.com>
2020-08-05 12:31:53 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs = require('hbs')
 const weather = require('./weather.js')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define Paths
 const publicDirPath = path.join(__dirname,'../public')
@@ -76,6 +77,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server running.")
 })
